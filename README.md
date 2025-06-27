ae253838ea82c2bdf53784d08c5b5c3e9af96996
Arjun Satarkar <me@arjunsatarkar.net>
2025-06-27 12:01:19 +0530

    Improve extraction

--- README.md
+++ README.md
@@ -5,12 +5,9 @@
 ```
 curl -o 'data/raw-wiktextract-data.jsonl.gz' 'https://kaikki.org/dictionary/raw-wiktextract-data.jsonl.gz'
 
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-
 ./extract_kana_words.py # Creates data/kana_words.json
 # You can delete data/raw-wiktextract-data.jsonl.gz now if you want
 
+npm install
 # TODO: run the webserver
 ```
--- extract_kana_words.py
+++ extract_kana_words.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from pathlib import Path
 import gzip
-import html
 import json
 import sys
 
@@ -21,45 +20,68 @@ KANA = char_ranges(
 OUT_PATH = Path("data/kana_words.json")
 RAW_DATA_FILE_PATH = Path("data/raw-wiktextract-data.jsonl.gz")
 UPDATE_STATUS_EVERY_WORDS = 10_000
+EXCLUDED_CATEGORIES = {
+    "Japanese derogatory terms",
+    "Japanese ethnic slurs",
+    "Japanese misspellings",
+    "Japanese offensive terms",
+    "Japanese swear words",
+    "Japanese vulgarities",
+}
+
+
+def log_status(lines_read_num: int, kana_words_added_num: int):
+    sys.stderr.write(
+        f"Read {lines_read_num} lines; found {kana_words_added_num} kana words.\r"
+    )
+
 
 with gzip.open(RAW_DATA_FILE_PATH, "r") as raw_data_file:
-    word_definitions = {}
-    lines_read_num = 0
-    kana_words_added_num = 0
+    words = set()
 
-    for line in raw_data_file:
-        lines_read_num += 1
-        if lines_read_num % UPDATE_STATUS_EVERY_WORDS == 0:
-            sys.stderr.write(
-                f"Read {lines_read_num} lines; found {kana_words_added_num} kana words.\r"
-            )
+    i = 0
+    for i, line in enumerate(raw_data_file):
+        if i % UPDATE_STATUS_EVERY_WORDS == 0:
+            log_status(i, len(words))
 
         word_data = json.loads(line)
         try:
             word = word_data["word"]
             lang_code = word_data["lang_code"]
+            has_romanization = bool(
+                next(
+                    (
+                        form["form"]
+                        for form in word_data["forms"]
+                        if "romanization" in form["tags"]
+                    ),
+                    None,
+                )
+            )
+            has_gloss = bool(word_data["senses"][0]["glosses"])
+            categories = word_data["categories"]
         except KeyError:
             continue
 
-        if not (lang_code == "ja" and all(c in KANA for c in word)):
+        if not (
+            lang_code == "ja"
+            and has_gloss
+            and has_romanization
+            and all(c in KANA for c in word)
+            and all(category not in EXCLUDED_CATEGORIES for category in categories)
+        ):
             continue
 
-        try:
-            glosses = [sense["raw_glosses"][0] for sense in word_data["senses"]]
-        except KeyError:
-            continue
-
-        definitions_html = "<ul>"
-        for gloss in glosses:
-            definitions_html += f"<li>{html.escape(gloss, quote=False)}</li>"
-        definitions_html += "</ul>"
-
-        if word not in word_definitions:
-            word_definitions[word] = definitions_html
-            kana_words_added_num += 1
+        words.add(word)
+    log_status(i, len(words))
 
 with open(OUT_PATH, "w") as out_file:
-    json.dump(word_definitions, out_file, ensure_ascii=False, indent=0, sort_keys=True)
+    json.dump(
+        sorted(words),
+        out_file,
+        ensure_ascii=False,
+        separators=(",", ":"),
+    )
 
 # Prevent our progress message from being overwritten by the shell prompt
 sys.stderr.write("\n")
