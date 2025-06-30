939c38a680b89d0f4f95bc4b21e0d6da95f49e29
Arjun Satarkar <me@arjunsatarkar.net>
2025-06-30 16:53:07 +0530

    Add meta description and OGP

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import license from "vite-plugin-license";
 import { compression } from 'vite-plugin-compression2'
+import handlebarsPlugin from '@yoichiro/vite-plugin-handlebars';
+import license from "vite-plugin-license";
 import path from "node:path";
 
+const APP_HOME = "https://practicekana.arjunsatarkar.net/"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,10 +23,19 @@ export default defineConfig({
     }),
     compression({
       include: /\.(html|xml|css|json|js|mjs|svg|yaml|yml|toml|ttf|txt)$/
-    })
+    }),
+    handlebarsPlugin({
+      partialsDirectoryPath: "partials",
+      // Despite the name, seems to work fine for multi-page apps
+      transformIndexHtmlOptions: {
+        context: () => ({
+          app_home: APP_HOME
+        })
+      }
+    }) as PluginOption
   ],
   define: {
-    __APP_HOME__: JSON.stringify("https://practicekana.arjunsatarkar.net/"),
+    __APP_HOME__: JSON.stringify(APP_HOME),
     __SOURCE_REPO__: JSON.stringify("https://github.com/arjunsatarkar/practice_kana_words")
   },
 })
