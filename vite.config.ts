0834f8f0655d9d976ee1f389ac31ad874a5a13a1
Arjun Satarkar <me@arjunsatarkar.net>
2025-07-15 13:37:02 +0530

    Simplify

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { createViteLicensePlugin } from 'rollup-license-plugin';
 import { imagetools } from 'vite-imagetools';
-import { name as projectName, repository } from './package.json';
+import { repository } from './package.json';
 
 const APP_HOME = "https://practicekana.arjunsatarkar.net/";
 
@@ -17,7 +17,6 @@ export default defineConfig({
 	define: {
 		__APP_HOME__: JSON.stringify(APP_HOME),
 		__REPO_URL__: JSON.stringify(repository.url),
-		__PROJECT_NAME__: JSON.stringify(projectName),
 	},
 	server: {
 		fs: {
